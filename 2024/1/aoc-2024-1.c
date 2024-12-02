a6a447a214ac8e85ef6554b02256ec98ba1e3b78
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2024-12-01 21:01:54 -0600

    finished part 1 of day 1

--- 2024/1/aoc-2024-1.c
+++ 2024/1/aoc-2024-1.c
@@ -1,36 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void append(int* array, int newval, int len) {
-    array = realloc(array, (len+1)*sizeof(*array));
-    array[len] = newval;
+void append(int** arr, int* newval, int len) {
+    *arr = realloc(*arr, (len+1)*sizeof(int));
+    (*arr)[len] = *newval;
+}
+
+int comp(const void* a,const void* b) {
+    return *(int*)a - *(int*)b;
+}
+
+void sort(int* arr, int len) {
+    qsort(arr, len, sizeof(int), comp);
 }
 
 int main(int argc, char *argv[]) {
-    int int1, int2;
+    int *int1 = malloc(sizeof(int));
+    int *int2 = malloc(sizeof(int));
 
     int len = 0;
     int* nums1 = malloc(sizeof(int));
     int* nums2 = malloc(sizeof(int));
 
-    while(EOF != fscanf(stdin, "%d  %d\n", &int1, &int2)) {
-        append(nums1, int1, len);
-        append(nums2, int2, len);
+    while(EOF != fscanf(stdin, "%d  %d\n", int1, int2)) {
+        append(&nums1, int1, len);
+        append(&nums2, int2, len);
         len++;
     }
 
-    for(int i = 0; i < len; i++) {
-        printf("%d", nums1[i]);
+    sort(nums1, len);
+    sort(nums2, len);
+   
+    int total = 0;
+    for (int i = 0; i < len; ++i) {
+        total += abs(nums1[i] - nums2[i]);
     }
-    printf("%s", "\n");
 
-
-    for(int i = 0; i < len; i++) {
-        printf("%d", nums2[i]);
-    }
-    printf("%s", "\n");
+    printf("%d\n", total);
     
-    free(nums1);
-    free(nums2);
+
+    //for (int i = 0; i < len; ++i) {
+    //    printf("%d", nums1[i]);
+    //}
+    //printf("\n");
+
+    //for (int i = 0; i < len; ++i) {
+    //    printf("%d", nums2[i]);
+    //}
+    //printf("\n");
 }
 
